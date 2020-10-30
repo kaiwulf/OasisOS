dbcc41005b1d12837648041254a3c23a6712cfd2
kaiwulf <eigenspaces@gmail.com>
2020-10-30 14:18:25 -0400

    script for building kernel and running it

---
+++ boot/build.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+grub-mkrescue -o myos.iso isodir
+qemu-system-i386 -cdrom myos.iso
