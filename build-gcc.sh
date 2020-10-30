86231b1838d9adf696e41c1e577ae32ada5c69bd
kaiwulf <eigenspaces@gmail.com>
2020-10-30 13:14:29 -0400

    compile c,c++ cross compilers only first

--- build-gcc.sh
+++ build-gcc.sh
@@ -7,7 +7,7 @@ export PATH="$PREFIX/bin:$PATH"
 which -- $TARGET-as || echo $TARGET-as is not in the PATH
 
 cd build-gcc
-../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=all --without-headers
+../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
 make all-target-libgcc
 make install-gcc
