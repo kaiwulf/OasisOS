420f64aca3ac9e7e1ff4f761f3c7a15b71b9a32f
kaiwulf <eigenspaces@gmail.com>
2020-11-06 10:55:11 -0500

    improving build system

--- sysroot/boot/build.sh
+++ sysroot/boot/build.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
+CRTBEGIN=/home/kai/.bin/cross/lib/gcc/i686-elf/10.2.0/crtbegin.o
+CRTEND=/home/kai/.bin/cross/lib/gcc/i686-elf/10.2.0/crtend.o
+
+i686-elf-as boot.s -o boot.o
+i686-elf-as x86_crti.s -o crti.o
+i686-elf-as x86_crtn.s -o crtn.o
+
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib crti.o CRTBEGIN boot.o kernel.o CRTEND crti.o -lgcc
 mv -f myos.bin isodir/boot/myos.bin
 grub-mkrescue -o myos.iso isodir
 qemu-system-i386 -cdrom myos.iso
